f73c6e6bdb2347028aee02b021a98ee340062e50
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:39:10 +0200

    chore: bump template

--- src/searchly/__init__.py
+++ src/searchly/__init__.py
@@ -1,7 +1,18 @@
+"""Searchly: Search tools for agents"""
+
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("searchly")
+__title__ = "Searchly"
+__description__ = "Search tools for agents"
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2025 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/searchly"
 
-__all__: list[str] = []
+__all__ = [
+    "__version__",
+]
