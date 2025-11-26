1133f4d839f17acc9f1ce80aa273b9536fc81bec
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-11-26 20:49:46 +0100

    chore: top-level exports

--- src/searchly/__init__.py
+++ src/searchly/__init__.py
@@ -1,11 +1,50 @@
-"""Searchly: main package.
+"""Searchly: Search tools for agents.
 
-Search tools for agents.
+Provides unified search interfaces across multiple search providers.
 """
 
 from __future__ import annotations
 
 from importlib.metadata import version
+
+from searchly.base import (
+    CountryCode,
+    LanguageCode,
+    NewsSearchProvider,
+    NewsSearchResponse,
+    NewsSearchResult,
+    WebSearchProvider,
+    WebSearchResponse,
+    WebSearchResult,
+)
+from searchly.config import (
+    BaseSearchProviderConfig,
+    BraveSearchConfig,
+    DataForSEOConfig,
+    ExaConfig,
+    JigsawStackConfig,
+    KagiConfig,
+    LinkUpConfig,
+    NewsSearchProviderConfig,
+    Search1Config,
+    SerperConfig,
+    SerpAPIConfig,
+    TavilyConfig,
+    WebSearchProviderConfig,
+    YouConfig,
+)
+from searchly.providers.brave_provider.client import AsyncBraveSearch
+from searchly.providers.dataforseo_provider.dataforseo import AsyncDataForSEOClient
+from searchly.providers.exa_provider.exa import AsyncExaClient
+from searchly.providers.jigsawstack_provider.jigsawstack import AsyncJigsawStackClient
+from searchly.providers.kagi_provider.client import AsyncKagiClient
+from searchly.providers.linkup_provider.client import AsyncLinkUpClient
+from searchly.providers.search1_provider.client import AsyncSearch1API
+from searchly.providers.serpapi_provider.client import AsyncSerpAPIClient
+from searchly.providers.serper_provider.client import AsyncSerperClient
+from searchly.providers.tavily_provider.client import AsyncTavilyClient
+from searchly.providers.you_provider.you import AsyncYouClient
+
 
 __version__ = version("searchly")
 __title__ = "Searchly"
@@ -17,5 +56,46 @@ __license__ = "MIT"
 __url__ = "https://github.com/phil65/searchly"
 
 __all__ = [
+    # Version
     "__version__",
+    # Protocols
+    "NewsSearchProvider",
+    "WebSearchProvider",
+    # Response models
+    "NewsSearchResponse",
+    "NewsSearchResult",
+    "WebSearchResponse",
+    "WebSearchResult",
+    # Type literals
+    "CountryCode",
+    "LanguageCode",
+    # Config base
+    "BaseSearchProviderConfig",
+    # Provider configs
+    "BraveSearchConfig",
+    "DataForSEOConfig",
+    "ExaConfig",
+    "JigsawStackConfig",
+    "KagiConfig",
+    "LinkUpConfig",
+    "Search1Config",
+    "SerperConfig",
+    "SerpAPIConfig",
+    "TavilyConfig",
+    "YouConfig",
+    # Config unions
+    "NewsSearchProviderConfig",
+    "WebSearchProviderConfig",
+    # Clients
+    "AsyncBraveSearch",
+    "AsyncDataForSEOClient",
+    "AsyncExaClient",
+    "AsyncJigsawStackClient",
+    "AsyncKagiClient",
+    "AsyncLinkUpClient",
+    "AsyncSearch1API",
+    "AsyncSerpAPIClient",
+    "AsyncSerperClient",
+    "AsyncTavilyClient",
+    "AsyncYouClient",
 ]
