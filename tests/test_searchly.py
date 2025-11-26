e698f938562885e075ee47b67a183bfd32e01577
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-11-26 22:25:17 +0100

    chore: test work

---
+++ tests/test_searchly.py
@@ -0,0 +1,8 @@
+"""Test nothing in order to make tests pass if all deselected."""
+
+from __future__ import annotations
+
+
+def test_nothing():
+    """Test nothing in order to make tests pass if all deselected."""
+    assert True
